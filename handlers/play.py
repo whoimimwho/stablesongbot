f405c8b1ee50c9e879c0ba9b7cd78e25776242a0
sangramghangale <81154807+sangramghangale@users.noreply.github.com>
2021-04-06 19:57:45 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -84,7 +84,7 @@ async def play(client: Client, message_: Message):
         res.delete
         m = await client.send_photo(
         chat_id=message_.chat.id,
-        photo="https://telegra.ph/file/7ffa8d18b9b7f1b51a81e.jpg",
+        photo="https://telegra.ph/file/bfd3e51f44415da8875f3.jpg",
         caption=f"Playing Your song Via  [✯𝗩𝗖𝗣𝗹𝗮𝘆✯](https://t.me/TeLeTiPsOfficialOnTopicChat).",
          ) 
         tgcalls.pytgcalls.join_group_call(message_.chat.id, file_path)
