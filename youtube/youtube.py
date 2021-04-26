fe513f5667029bc18ba41f04185647384e7cf246
whoimimwho <79775567+whoimimwho@users.noreply.github.com>
2021-04-26 12:42:37 +0530

    Update youtube.py

--- youtube/youtube.py
+++ youtube/youtube.py
@@ -14,11 +14,11 @@ ydl = YoutubeDL(ydl_opts)
 
 def download(url: str) -> str:
     info = ydl.extract_info(url, False)
-    duration = round(info["duration"] / 60)
+    duration = round(info["duration"] / 180)
 
     if duration > DURATION_LIMIT:
         raise DurationLimitError(
-            f"✯𝗖𝗵𝗼𝗰𝗼𝗹𝗮𝘁𝘆𝗤𝘂𝗲𝗲𝗻𝗕𝗼𝘁✯=Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)"
+            f"Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)"
         )
 
     ydl.download([url])
